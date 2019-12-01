17bbf7527e0191e0b63fa706813ad706427ed201
Mikhil Nagarale <mikhil.nagarale@gmail.com>
2019-12-01 19:23:22 +0000

    updating flixbus_tweets.py for API check status

--- dags/flixbus_tweets.py
+++ dags/flixbus_tweets.py
@@ -11,6 +11,7 @@ config.read('/home/airflow/flixbus/conf/conf.py')
 import download_data
 import transform_tweets
 import get_access_token 
+import check_api_status
 
 current_date = datetime.now().strftime("%Y%m%d")
 
@@ -32,11 +33,13 @@ parsed_file_path = config['DEFAULT']['data_dir']+"/"+parsed_file_name
 hdfs_parsed_file_path = config['DEFAULT']['hdfs_tmp']+"/"+parsed_file_name
 
 with DAG('flixbus_tweets_dag_v2', start_date=datetime(2018, 10, 1), schedule_interval="@daily", default_args=DAG_DEFAULT_ARGS, catchup=False) as dag:
+	check_api_status_task = PythonOperator(task_id='check_api_status_task',python_callable=check_api_status.main)
 	get_access_token_task = PythonOperator(task_id='get_access_token_task',python_callable=get_access_token.main)
 	download_tweets_task = PythonOperator(task_id='download_tweets_task',python_callable=download_data.main)
 	transform_tweets_task = PythonOperator(task_id='transform_tweets_task',python_callable=transform_tweets.main)
 	copy_to_hdfs_task = BashOperator(task_id='copy_to_hdfs_task', bash_command="hadoop fs -put -f "+parsed_file_path+" /tmp")
 	load_hive_table_task = HiveOperator(task_id='load_hive_table_task',hql='LOAD DATA INPATH \'{}\' into table flixbus_tweets PARTITION(load_date=\'{}\')'.format(hdfs_parsed_file_path,current_date))
-	get_access_token_task >> download_tweets_task >> transform_tweets_task >> copy_to_hdfs_task >> load_hive_table_task
+
+	check_api_status_task >> get_access_token_task >> download_tweets_task >> transform_tweets_task >> copy_to_hdfs_task >> load_hive_table_task
 	
 		
